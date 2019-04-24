7f96c9c243399643853ace8949ce200bcaa353eb
JianweiZhang <jwzhang86@gmail.com>
2019-04-23 23:15:53 -0400

    computer broken

--- Security/src/main/java/com/zjw/springsecurity/demo/config/DemoSecurityConfig.java
+++ Security/src/main/java/com/zjw/springsecurity/demo/config/DemoSecurityConfig.java
@@ -34,7 +34,9 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/showMyLoginPage")
                 .loginProcessingUrl("/authenticateTheUser")
-                .permitAll();
+                .permitAll()
+                .and()
+                .logout().permitAll();
 
     }
 
